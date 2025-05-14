c65285770878feb7f43de2ae8d48076a8fc5d4bd
Dani-807 <daniellegansoen07@gmail.com>
2025-05-14 12:51:28 -0400

    Create draw.js

---
+++ draw.js
@@ -0,0 +1,29 @@
+// We can add more screens later (if needed)
+
+function draw() {
+
+  background("black");
+
+  switch (currentScreen) {
+		case STATE_LOAD:
+			drawLoad(); // Draw the load screen
+			break;
+			
+    case STATE_START:
+      drawStart();  // Draw the start screen
+      break;
+
+    case STATE_PLAY:
+      drawPlay();   // Draw the play screen 
+      break;
+
+    case STATE_PAUSE:
+      drawPause();  // Draw the pause screen
+      break;
+
+    case STATE_END:
+      drawEnd();    // Draw the end screen
+      break;
+  }
+
+}
