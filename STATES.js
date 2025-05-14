ccf2940554c231cbfa3e7914f060843e8077ce1d
Dani-807 <daniellegansoen07@gmail.com>
2025-05-14 17:47:12 -0400

    Update STATES.js

--- STATES.js
+++ STATES.js
@@ -1,1 +1,10 @@
+//screen states
+const STATE_LOAD = 'load';
+const STATE_START = 'start';
+const STATE_PLAY = 'play';
+const STATE_PAUSE = 'pause';
+const STATE_END = 'end';
+const STATE_INSTRUCT = 'instructions';
 
+//initial screen state
+let currentScreen = STATE_LOAD;
