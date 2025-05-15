7e1748d9e90b8a04150b7b5cf41622ed6a48c841
Raymund Gloria <Rayray221208@gmail.com>
2025-05-15 13:02:30 -0400

    Update Play.js
    added circle that the player will control

--- Play.js
+++ Play.js
@@ -1,24 +1,29 @@
-function drawPlay () {
+function drawPlay() {
 
-    background('rgb(250,184,206)');
+	background('rgb(250,184,206)');
 	fill('black')
 	textAlign(CENTER, CENTER);
 	textSize(width * 0.02)
-	text("You are now playing", width / 2, height / 2);
-	
+
+
 	// Button parameters
 	let xCenter = width - 100;
 	let yBack = height * 0.9;
 	let bW = width * 0.10;
 	let bH = height * 0.10;
-	
+
 	rectMode(CENTER);
-	
+
 	// Back button
 	fill('rgb(219,219,255)');
 	rect(xCenter, yBack, bW, bH, 20);
-	
+
 	fill('black')
 	text("Back", xCenter, yBack)
+
+	// player controls circle
+	fill('white')
+	circle(mouseX, mouseY, 50)
+
 
 }
