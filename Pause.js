8aa2854fffba2cf77669e7b0a867abe82014868a
Dani-807 <daniellegansoen07@gmail.com>
2025-05-23 12:55:46 -0400

    Update Pause.js

--- Pause.js
+++ Pause.js
@@ -1,3 +1,20 @@
 function drawPause(){
   
+  background('rgb(195,248,221)');
+	textAlign(CENTER, CENTER);
+	textSize(width * 0.02);
+	
+	// Back to start button
+	let xCenter = width - 100;
+	let yBack = height * 0.9;
+	let bW = width * 0.10;
+	let bH = height * 0.10;
+	
+	// Back button
+	fill('rgb(219,219,255)');
+	rect(xCenter, yBack, bW, bH, 20);
+	
+	fill('black');
+	text("Back", xCenter, yBack);
+  
 }
